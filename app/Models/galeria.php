ac4eb256bc9448bb3da8c9dad7a0b34797b955e4
Santiago Godoy <castillogodoysantiago@gmail.com>
2025-09-26 18:51:58 -0500

    Correcion

--- app/Models/galeria.php
+++ app/Models/galeria.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class galeria extends Model
+class Galeria extends Model
 {
     use HasFactory;
 
